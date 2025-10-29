813c9b5e061b2d0f926b4660d90d8f687152d218
AladdinBelhaj <alabelhaj2002@gmail.com>
2025-10-29 19:43:33 +0100

    remove comments

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ import publicationRoutes from './routes/publicationRoutes.js';
 
 const app = express();
 
-app.use(express.json()); // for parsing JSON
-app.use(express.urlencoded({ extended: true })); // for parsing form-data text fields
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/publications', publicationRoutes);
 app.use('/uploads', express.static('uploads'));
